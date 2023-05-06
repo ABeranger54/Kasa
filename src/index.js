55c71ffd1d1ed2696a6edf120d4d491b13ba4e1b
Alexandre Beranger <alexandre.beranger54@gmail.com>
2023-05-06 07:00:29 +0200

    - Changement routeur basename

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import Footer from './components/Footer';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router basename='/Kasa'>
+    <Router basename={process.env.PUBLIC_URL}>
       <Header />
       <AppRouter />
       <Footer />
