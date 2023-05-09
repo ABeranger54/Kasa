4142c898974b535138bed5941114701c1d2d61e5
Alexandre Beranger <alexandre.beranger54@gmail.com>
2023-05-09 17:20:19 +0200

    - Les covers sont affichées en tant qu'images dans Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,18 @@
 import '../styles/Card.css';
 import { Link } from 'react-router-dom';
+import annonces from "../data/annonces.json"
 
 function Card(props){
+
+    const location = annonces.filter(loc => loc.id === props.id)[0];
+
     return (
         <div className="card">
             <Link to={"/location/" + props.id}>
-                <h2>{props.title}</h2>
+                <div className="cardContainer">
+                    <img src={location.cover} alt={location.title} />
+                    <div className="cardTitle">{location.title}</div>
+                </div>
             </Link>
         </div>
     );
--- src/pages/Location.js
+++ src/pages/Location.js
@@ -47,7 +47,6 @@ function Location(){
             </main>
         )
      }
-    
 }
 
 export default Location;
